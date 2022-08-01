768efb65b211a584b296daa754f281244702413c
dcen7 <mdcengiz@gmail.com>
2022-08-01 16:14:07 -0500

    make network call for trending movies and get result with Result type and create Movie model

---
+++ Netflix Clone/Managers/APICallers.swift
@@ -0,0 +1,40 @@
+//
+//  APICallers.swift
+//  Netflix Clone
+//
+//  Created by Mehmet Deniz Cengiz on 8/1/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "f846c16d5a7ee78ba3c0399a19758ee9"
+    static let baseURL = "https://api.themoviedb.org"
+}
+
+enum APIError: Error {
+    case failedTogetData
+}
+
+class APICaller {
+    static let shared = APICaller()
+    
+    func getTrendingMovies(completion: @escaping (Result<[Movie], Error>) -> Void) {
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(TrendingMoviesReponse.self, from: data)
+                completion(.success(results.results))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+}
