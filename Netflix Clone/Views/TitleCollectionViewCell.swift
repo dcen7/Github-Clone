ffa71ab0e0ff7cfef280a47b2ce72a406945a5e6
dcen7 <mdcengiz@gmail.com>
2022-08-02 07:28:30 -0500

    finish all types of movies api calls

---
+++ Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix Clone
+//
+//  Created by Mehmet Deniz Cengiz on 8/2/22.
+//
+
+import UIKit
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    
+}
