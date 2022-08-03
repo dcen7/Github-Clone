bf0e4a7cd14e4a34a4ad6215b4460621845f202e
dcen7 <mdcengiz@gmail.com>
2022-08-03 07:14:48 -0500

    add titleviewcell for upcoming titles and create upcomingview control with upcomingtableviewa , create titleviewmodel

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Mehmet Deniz Cengiz on 8/2/22.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
