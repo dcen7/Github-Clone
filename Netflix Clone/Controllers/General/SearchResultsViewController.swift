d00452e65dcd30298bac950104208c2c3f223e0c
dcen7 <mdcengiz@gmail.com>
2022-08-03 07:45:33 -0500

    create searchviewcontroller and fetch data

---
+++ Netflix Clone/Controllers/General/SearchResultsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by Mehmet Deniz Cengiz on 8/3/22.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+  
+}
