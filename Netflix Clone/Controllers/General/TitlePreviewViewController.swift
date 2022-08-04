5e9b3e495d0626cf964874f08247915687932f74
dcen7 <mdcengiz@gmail.com>
2022-08-04 09:06:58 -0500

    use delegate pattern to notify homeviewcontroller by tapping on the cell

---
+++ Netflix Clone/Controllers/General/TitlePreviewViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TitlePreviewViewController.swift
+//  Netflix Clone
+//
+//  Created by Mehmet Deniz Cengiz on 8/3/22.
+//
+
+import Foundation
---
+++ Netflix Clone/ViewModels/TitlePreviewViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  TitlePreviewViewModel.swift
+//  Netflix Clone
+//
+//  Created by Mehmet Deniz Cengiz on 8/3/22.
+//
+
+import Foundation
