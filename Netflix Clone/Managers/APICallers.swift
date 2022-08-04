c82fc96628532cc0fe1ac47627b61429be9dee85
dcen7 <mdcengiz@gmail.com>
2022-08-04 09:25:27 -0500

    use delegate pattern to notify homeviewcontroller by tapping on the cell

--- Netflix Clone/Managers/APICallers.swift
+++ Netflix Clone/Managers/APICallers.swift
@@ -10,6 +10,8 @@ import Foundation
 struct Constants {
     static let API_KEY = "f846c16d5a7ee78ba3c0399a19758ee9"
     static let baseURL = "https://api.themoviedb.org"
+    static let YoutubeAPI_KEY = "AIzaSyAhxyEc_BC-f-2mxpa8ytylpZbu7r9FtUg"
+    static let YoutubeBaseURL = "https://youtube.googleapis.com/youtube/v3/search?"
 }
 
 enum APIError: Error {
@@ -132,7 +134,7 @@ class APICaller {
         
         task.resume()
     }
-    // https://api.themoviedb.org/3/search/movie?api_key={api_key}&query=Jack+Reacher
+    
     func search(with query: String, completion: @escaping (Result<[Title], Error>) -> Void) {
         guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
         guard let url = URL(string: "\(Constants.baseURL)/3/search/movie?api_key=\(Constants.API_KEY)&query=\(query)") else { return }
@@ -147,6 +149,28 @@ class APICaller {
                 completion(.success(results.results))
             } catch {
                 completion(.failure(APIError.failedTogetData))
+            }
+        }
+        
+        task.resume()
+    }
+    
+ 
+    func getMovie(with query: String, completion: @escaping (Result<VideoElement, Error>) -> Void) {
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else { return }
+        guard let url = URL(string: "\(Constants.YoutubeBaseURL)q=\(query)&key=\(Constants.YoutubeAPI_KEY)") else { return }
+        
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            do {
+                let results = try JSONDecoder().decode(YoutubeSearchResponse.self, from: data)
+                completion(.success(results.items[0]))
+                
+            } catch {
+                print(error.localizedDescription)
             }
         }
         
